23b2961813269a0d4aa26ad7febd24c5b42734dd
dewordman <54588248+dewordman@users.noreply.github.com>
2020-06-03 17:32:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <h1>My first PHP page</h1>
 
 <?php
-echo "Hello World, this is Tunde Ajibola with HNG-06341 using javascript for stage 2 task";
+echo "Hello World, this is Tunde Ajibola with HNG-06341 using php for stage 2 task";
 ?>
 
 </body>
