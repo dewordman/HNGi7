00c1b61ee16c1b8242ffdd71099a2f5b01fd6904
dewordman <54588248+dewordman@users.noreply.github.com>
2020-06-04 09:39:23 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const result = () => {
+    const Info = {
+        Name: " Tunde Ajibola",
+        Hng_ID: "HNG-06341" ,
+        Lang_used: " Javascript"
+    }
+    const text = ` Hello World,  I am  ${Info.Name } with the ID of  ${Info.Hng_ID } I am currently using
+     ${ Info.Lang_used } to code`;
+
+      console.log(text);
